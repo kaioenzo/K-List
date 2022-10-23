975e8f81ede9b1e489bc701f2374a0a50508e2f2
Kaio Enzo <kaioenzobr@gmail.com>
2022-10-23 00:34:04 -0300

    Create base screens

---
+++ src/screens/Account.tsx
@@ -0,0 +1,10 @@
+import { Box, Text } from "native-base";
+import React from "react";
+
+export default function Account() {
+  return (
+    <Box>
+      <Text>Account page</Text>
+    </Box>
+  );
+}
---
+++ src/screens/CreateTask.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "native-base";
+import React from "react";
+export default function Account() {
+  return (
+    <Box>
+      <Text>CreateTask page</Text>
+    </Box>
+  );
+}
