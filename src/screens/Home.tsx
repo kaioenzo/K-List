b5e609c133da074dbc038fd433deed2fe7a935ac
Kaio Enzo <kaioenzobr@gmail.com>
2022-11-04 00:16:33 -0300

    Change nested routes

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,31 +1,66 @@
+import { Entypo, Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import Account from "./Account";
-import CreateTask from "./CreateTask";
 import HomeTask from "./HomeTask";
+import TaskRoutes from "./TaskRoutes";
 
 export default function Home() {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { height: 70 },
+      }}
+    >
       <Tab.Screen
         name="Home"
         component={HomeTask}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name="home" size={size + 10} color={color} />;
+          },
+        }}
       />
       <Tab.Screen
-        component={CreateTask}
-        name="CreateTask"
+        component={TaskRoutes}
+        name="TaskRoutes"
         options={{
           headerShown: false,
           title: "",
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <Ionicons
+                style={{
+                  position: "absolute",
+                  bottom: -10,
+                }}
+                name="ios-add-circle"
+                color={color}
+                size={size + 40}
+              />
+            );
+          },
+          tabBarActiveTintColor: "#6135BC",
         }}
       />
       <Tab.Screen
         name="Account"
         component={Account}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <MaterialIcons
+                name="account-circle"
+                size={size + 10}
+                color={color}
+              />
+            );
+          },
+        }}
       />
     </Tab.Navigator>
   );
