d99fe5012f3304feb00fb0948fac39f3a4da5d58
Kaio Enzo <kaioenzobr@gmail.com>
2022-12-18 19:34:09 -0300

    fix eslint no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ["@react-native-community", "eslint-config-prettier"]
+  extends: ["@react-native-community", "eslint-config-prettier"],
+  rules: {
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
 };
