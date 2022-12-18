037bebb3383200fd85a968765230313aa838bcd7
Kaio Enzo <kaioenzobr@gmail.com>
2022-12-18 19:31:34 -0300

    Add complete text in card of notes

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Checkbox, Text } from "native-base";
+import { Box, Button, Checkbox, Text } from "native-base";
 import React, { useState } from "react";
 import { setDoneState } from "../services/NotesService";
 import { NotePropsFromDB } from "../types";
@@ -8,16 +8,15 @@ function NoteCard(data: NotePropsFromDB) {
   const [groupValues, setGroupValues] = useState([
     data.done === `TRUE` ? `TRUE` : `FALSE`,
   ]);
-  console.log(`on render:${groupValues}`);
+
   return (
-    <Box flex={1} flexDirection="row" justifyContent="flex-start" mx={8}>
+    <Box flex={1} flexDirection="row" justifyContent="flex-start" mx={8} my={4}>
       <Box>
         <Text>{data.date}</Text>
         <Checkbox.Group
           accessibilityLabel="check your activitie to did"
           onChange={(values) => {
             setGroupValues(values);
-            console.log(`on click:${groupValues}`);
             if (groupValues.find((v) => v === `TRUE`)) {
               setDoneState(false, data.id);
               setDone(false);
@@ -36,36 +35,25 @@ function NoteCard(data: NotePropsFromDB) {
           />
         </Checkbox.Group>
       </Box>
-      <Box ml={4} flexDirection="column" flexGrow={1}>
+      <Box ml={4} flexDirection="column" flexGrow={1} shadow={2}>
         <Text>{data.title}</Text>
         <Text>{data.description}</Text>
-      </Box>
-      <Box>
-        {/* <Popover
-        trigger={(triggerProps) => {
-          return (
-            <Icon {...triggerProps} as={<AntDesign name="checkcircle" />} />
-          );
-        }}
-      >
-        <Popover.Content accessibilityLabel="Delete Customerd" w="56">
-          <Popover.Arrow />
-          <Popover.CloseButton />
-          <Popover.Header>Delete Customer</Popover.Header>
-          <Popover.Body>
-            This will remove all data relating to Alex. This action cannot be
-            reversed. Deleted data can not be recovered.
-          </Popover.Body>
-          <Popover.Footer justifyContent="flex-end">
-            <Button.Group space={2}>
-              <Button colorScheme="coolGray" variant="ghost">
-                Cancel
-              </Button>
-              <Button colorScheme="danger">Delete</Button>
-            </Button.Group>
-          </Popover.Footer>
-        </Popover.Content>
-      </Popover> */}
+        {done && (
+          <Button
+            position="absolute"
+            alignSelf="flex-end"
+            al
+            size="xs"
+            p={1}
+            borderRadius="full"
+            bgColor="#01cb48"
+            mt={8}
+          >
+            <Text color={"white"} fontSize="xs" mx={1}>
+              Complete
+            </Text>
+          </Button>
+        )}
       </Box>
     </Box>
   );
